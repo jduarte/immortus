a1b80b76b602a60646fe6d358ad7f3eec219376f
José Duarte <jose.fduarte@gmail.com>
2015-05-18 18:45:28 +0100

    Refactored Tracking Strategy testing and finders

--- lib/immortus/strategy_finder.rb
+++ lib/immortus/strategy_finder.rb
@@ -2,48 +2,40 @@ module Immortus
   class StrategyNotFound < StandardError; end
 
   class StrategyFinder
-    MAP_ACTIVE_JOB_ADAPTER_TO_DEFAULT_STRATEGY = {
-      delayed_job: :delayed_job_active_record,
-      backburner: :not_implemented,
-      qu: :not_implemented,
-      que: :not_implemented,
-      queue_classic: :not_implemented,
-      resque: :not_implemented,
-      sidekiq: :not_implemented,
-      sneakers: :not_implemented,
-      sucker_punch: :not_implemented,
-      inline: :not_implemented,
-      test: :empty
+    ACTIVE_JOB_INFERRED_STRATEGIES = {
+      delayed_job:    :delayed_job_active_record_strategy,
+      backburner:     :not_implemented,
+      qu:             :not_implemented,
+      que:            :not_implemented,
+      queue_classic:  :not_implemented,
+      resque:         :not_implemented,
+      sidekiq:        :not_implemented,
+      sneakers:       :not_implemented,
+      sucker_punch:   :not_implemented,
+      inline:         :not_implemented,
+      test:           :empty
     }
 
-    MAP_ACTIVE_JOB_ADAPTER_TO_REQUIREMENTS = {
-      delayed_job: 'delayed_job_active_record',
-      backburner: 'backburner',
-      qu: 'qu',
-      que: 'que',
-      queue_classic: 'queue_classic',
-      resque: ['resque', 'redis'],
-      sidekiq: 'sidekiq',
-      sneakers: 'sneakers',
-      sucker_punch: 'sucker_punch',
-      inline: nil,
-      test: nil
+    ADAPTER_REQUIREMENTS = {
+      delayed_job:    'delayed_job_active_record',
+      backburner:     'backburner',
+      qu:             'qu',
+      que:            'que',
+      queue_classic:  'queue_classic',
+      resque:         ['resque', 'redis'],
+      sidekiq:        'sidekiq',
+      sneakers:       'sneakers',
+      sucker_punch:   'sucker_punch',
+      inline:         nil,
+      test:           nil
     }
 
     def self.find
       strategy = immortus_config_strategy || active_job_adapter_default_strategy
-      raise StrategyNotFound, 'Strategy does not seems to be setted' if strategy.blank?
-      if strategy.is_a?(Class)
-        strategy
-      elsif strategy.is_a?(Symbol)
-        begin
-          "#{strategy.to_s.camelize}".constantize
-        rescue
-          try_to_find_class_by_adding_default_namespace strategy
-        end
-      else
-        raise StrategyNotFound, 'Strategy Not Found'
-      end
+      raise StrategyNotFound, 'Strategy is not set in the configuration' if strategy.blank?
+
+      return strategy if strategy.is_a?(Class)
+      load_strategy_class(strategy)
     end
 
     private
@@ -53,28 +45,27 @@ module Immortus
     end
 
     def self.active_job_adapter_default_strategy
-      sym = ::Rails.application.config.active_job.queue_adapter
+      aj_queue_adapter = ::Rails.application.config.active_job.queue_adapter
 
-      strategy = try_to_find_class_by_adding_default_namespace MAP_ACTIVE_JOB_ADAPTER_TO_DEFAULT_STRATEGY[sym]
+      strategy = load_strategy_class(ACTIVE_JOB_INFERRED_STRATEGIES[aj_queue_adapter])
 
-      Array(MAP_ACTIVE_JOB_ADAPTER_TO_REQUIREMENTS[sym]).each do |requirement|
+      Array(ADAPTER_REQUIREMENTS[aj_queue_adapter]).compact.each do |requirement|
         require requirement
       end
 
       strategy
     end
 
-    def self.try_to_find_class_by_adding_default_namespace(strategy_class_sym)
-      return nil if strategy_class_sym.blank?
+    def self.load_strategy_class(name)
+      class_name = "Immortus::TrackingStrategy::#{name.to_s.camelize}"
+      non_namespace = "TrackingStrategy::#{name.to_s.camelize}"
 
-      strategy_class_name = strategy_class_sym.to_s
-      strategy_class_name << '_strategy' unless strategy_class_name.ends_with? '_strategy'
-
-      begin
-        "Immortus::TrackingStrategy::#{strategy_class_name.camelize}".constantize
-      rescue
-        raise StrategyNotFound, 'Could not find strategy in Immortus::TrackingStrategy namespace, please specify full namespace ex: MyApp::MyOverrides::MyCustomStrategy'
+      [class_name, non_namespace].each do |k_name|
+        class_exists = !!Module.const_get(k_name) rescue false
+        return k_name.constantize if class_exists
       end
+
+      raise StrategyNotFound, "Could not find Strategy. Tried `#{class_name}` and `#{non_namespace}`"
     end
   end
 end
