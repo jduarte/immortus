5f38998931ae00e46353d6cc552e9f4bffa1dfe5
Nuno Duarte <n.duarte@runtime-revolution.com>
2015-05-27 15:41:20 +0100

    add "some" details

--- lib/immortus/renders.rb
+++ lib/immortus/renders.rb
@@ -4,7 +4,7 @@ module ActionController
       if job.try('job_id')
         render json: { job_id: job.job_id, job_class: job.class.name }
       else
-        render json: { job_id: job.job_id }, status: 500
+        render json: {}, status: 500
       end
     end
   end
