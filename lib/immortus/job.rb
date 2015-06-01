d2f3241eae81f1429cf692946480a45903fd50f8
Nuno Duarte <n.duarte@runtime-revolution.com>
2015-06-01 11:15:12 +0100

    remove ActiveSupport::Concern dependency

--- lib/immortus/job.rb
+++ lib/immortus/job.rb
@@ -1,11 +1,36 @@
 module Immortus
   module Job
-    extend ActiveSupport::Concern
-
     # global config (initializer)
     cattr_accessor :tracking_strategy
 
-    class_methods do
+    def self.included(base)
+      base.extend ClassMethods
+      base.class_eval do
+        after_enqueue :tracker_create
+        before_perform :tracker_mark_started
+        after_perform :tracker_finish_job
+
+        def strategy
+          @strategy ||= self.class.strategy_class.new
+        end
+
+        private
+
+        def tracker_create
+          self.strategy.job_enqueued(self.job_id) if self.strategy.respond_to?(:job_enqueued)
+        end
+
+        def tracker_mark_started
+          self.strategy.job_started(self.job_id) if self.strategy.respond_to?(:job_started)
+        end
+
+        def tracker_finish_job
+          self.strategy.job_finished(self.job_id) if self.strategy.respond_to?(:job_finished)
+        end
+      end
+    end
+
+    module ClassMethods
       attr_reader :inline_tracking_strategy
 
       def strategy_class
@@ -19,30 +44,6 @@ module Immortus
       # inline config (job)
       def tracking_strategy(strategy)
         @inline_tracking_strategy = strategy
-      end
-    end
-
-    included do
-      after_enqueue :tracker_create
-      before_perform :tracker_mark_started
-      after_perform :tracker_finish_job
-
-      def strategy
-        @strategy ||= self.class.strategy_class.new
-      end
-
-      private
-
-      def tracker_create
-        self.strategy.job_enqueued(self.job_id) if self.strategy.respond_to?(:job_enqueued)
-      end
-
-      def tracker_mark_started
-        self.strategy.job_started(self.job_id) if self.strategy.respond_to?(:job_started)
-      end
-
-      def tracker_finish_job
-        self.strategy.job_finished(self.job_id) if self.strategy.respond_to?(:job_finished)
       end
     end
   end
